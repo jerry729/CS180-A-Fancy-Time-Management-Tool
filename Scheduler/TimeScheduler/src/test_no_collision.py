3ea0ef95fc7c9509ea00c15d364b630bd379f719
nks738 <nathankshin@gmail.com>
2023-02-19 23:00:41 -0800

    fixed for readability

--- Scheduler/TimeScheduler/src/test_no_collision.py
+++ Scheduler/TimeScheduler/src/test_no_collision.py
@@ -1,37 +1,41 @@
 import random
-
+    
 def test_no_collisions():
-    # Create a new schedule with random tasks
+    #create a new schedule with random tasks
     schedule = Schedule()
-    schedule.generate_random_tasks()
+    numTasks = random.randint(0,10)
+    defaultTasks = schedule.generate_random_tasks(numTasks)
+    schedule.place_tasks(defaultTasks)
+    
+    for default in numTasks
+        assert default in schedule.tasks
 
-    # Generate a second set of tasks to place in the schedule
-    new_tasks = schedule.generate_random_tasks()
+    #generate a second set of tasks to place in the schedule
+    addTasks = random.randint(0,10)
+    new_tasks = schedule.generate_random_tasks(addTasks)
 
-    # Place the new tasks in the schedule using the pre-written function
+    #uses place_tasks to place new tasks
     schedule.place_tasks(new_tasks)
 
-    # Check that the new tasks have been placed in the schedule correctly
+    #check that the new tasks have been placed in the schedule correctly
     for task in new_tasks:
         assert task in schedule.tasks
 
-    # Check that there are no time collisions between the tasks
+    #check that there are no time collisions between the tasks
     for task1 in schedule.tasks:
         for task2 in schedule.tasks:
             if task1 != task2:
                 assert not task1.time_overlap(task2)
 
-def __init__(self):
-    self.tasks = []
 
-def generate_random_tasks(self, n=10):
+def generate_random_tasks(n):
     for i in range(n):
-        # Generate a task with a random start time and duration
-        start_time = random.randint(0, 720)
-        duration = random.randint(5, 60)
+        #generate a task with a random start time and duration
+        start_time = random.randint(0, 1440) #represent 24 hrs in minuts
+        duration = random.randint(30, 120) #each task given time range of 30-120
         task = Task(start_time, duration)
 
-        # Add the task to the list of tasks
+        #add the task to the list of tasks
         self.tasks.append(task)
 
     return self.tasks
