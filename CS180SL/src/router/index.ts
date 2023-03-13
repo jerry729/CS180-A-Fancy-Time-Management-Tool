16eaeaa5fa42a319860a892c0874c14ad2378a02
nks738 <nathankshin@gmail.com>
2023-03-13 13:57:25 -0700

    imported MakeTeam view

--- CS180SL/src/router/index.ts
+++ CS180SL/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import LoginView from "@/views/LoginView.vue";
 import RegisterView from "@/views/RegisterView.vue";
+import MakeTeam from "@/views/MakeTeam.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: "/register",
       name: "Register",
       component: RegisterView,
+    },
+    {
+      path:"/team",
+      name: "MakeTeam",
+      component: MakeTeam,
     },
   ],
 });
