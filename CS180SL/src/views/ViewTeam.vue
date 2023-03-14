9324b76d87e67a50694915aed84d6ef9943819f3
nks738 <nathankshin@gmail.com>
2023-03-13 17:54:18 -0700

    changed design

--- CS180SL/src/views/ViewTeam.vue
+++ CS180SL/src/views/ViewTeam.vue
@@ -1,33 +1,36 @@
 <template>
-  <div class="team-page">
-    <h1 class="title">My Team</h1>
-    <ul class="members">
-      <li v-for="member in team" :key="member.id" class="member">
-        <h3 class="name">{{ member.name }}</h3>
-        <p class="role">{{ member.role }}</p>
-        <button class="remove-btn" @click="removeMember(member.id)">Remove</button>
-      </li>
-      <li v-if="team.length < 4 && !showAddMember" class="add-member">
-        <h3 class="name"><a @click="showAddMember = true">+ Add New Member</a></h3>
-      </li>
-      <li v-if="showAddMember" class="add-member-form">
-        <h3 class="name">Add New Member</h3>
-        <form @submit.prevent="addMember">
-          <div class="form-item">
-            <label for="name">Name:</label>
-            <input type="text" id="name" v-model="newMember.name" required>
-          </div>
-          <div class="form-item">
-            <label for="role">Role:</label>
-            <input type="text" id="role" v-model="newMember.role" required>
-          </div>
-          <div class="form-item">
-            <button type="submit">Add Member</button>
-            <button type="button" @click="cancelAddMember">Cancel</button>
-          </div>
-        </form>
-      </li>
-    </ul>
+  <div class ="page-container">
+    <div class="team-page">
+      <h1 class="title">My Team</h1>
+      <ul class="members">
+        <li v-for="member in team" :key="member.id" class="member">
+          <h3 class="name">{{ member.name }}</h3>
+          <p class="role">{{ member.role }}</p>
+          <button class="remove-btn" @click="removeMember(member.id)">Remove</button>
+        </li>
+        <li v-if="team.length < 4 && !showAddMember" class="add-member">
+          <h3 class="name" color = "blue"><a @click="showAddMember = true">+ Add New Member</a></h3>
+        </li>
+        <li v-if="showAddMember" class="add-member-form">
+          <h3 class="name">Add New Member</h3>
+          <form @submit.prevent="addMember">
+            <div class="form-item">
+              <label for="name">Name:</label>
+              <input type="text" id="name" v-model="newMember.name" required>
+            </div>
+            <div class="form-item">
+              <label for="role">Role:</label>
+              <input type="text" id="role" v-model="newMember.role" required>
+            </div>
+            <div class="form-item">
+              <button type="submit">Add Member</button>
+              <button type="button" @click="cancelAddMember">Cancel</button>
+            </div>
+          </form>
+        </li>
+      </ul>
+      <router-link to="/" class="back-btn">&#8592; Back to Schedule</router-link>
+    </div>
   </div>
 </template>
 
@@ -90,158 +93,171 @@ export default defineComponent({
 });
 </script>
 <style scoped>
-.team-page {
-  background-color: #f9c8a6;
-  color: #383838;
+.view-team-container {
+  width: 100%;
+  height: 100%;
   display: flex;
-  flex-direction: column;
-  align-items: center;
   justify-content: center;
-  min-height: 100vh;
-  padding: 2rem;
+  align-items: center;
+}
+body {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, sans-serif;
+  font-size: 16px;
+  color: black;
+  background-color: #f2f2f2;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  margin: 0;
+  padding: 0;
+  font-weight: normal;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+/* Header styles */
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  padding: 20px;
+}
+
+.logo {
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.nav-links {
+  display: flex;
+  align-items: center;
+}
+
+.nav-link {
+  margin-left: 20px;
+}
+
+/* Team page styles */
+.team-page {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 40px;
+  background-color: #fff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
 }
 
 .title {
-  font-size: 3rem;
-  margin-bottom: 2rem;
+  font-size: 36px;
+  font-weight: bold;
+  margin-bottom: 40px;
   text-align: center;
 }
 
 .members {
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  width: 100%;
+  margin: -10px;
 }
 
 .member {
-  background-color: #d1eecc;
-  border-radius: 1rem;
-  margin: 1rem;
-  padding: 2rem;
-  text-align: center;
-  transition: all 0.2s ease;
-  flex: 0 1 calc(25% - 2rem);
-  max-width: calc(25% - 2rem);
-}
-
-.member:hover {
-  transform: translateY(-0.5rem);
+  flex: 1 0 25%;
+  margin: 10px;
+  padding: 20px;
+  background-color: #f9f9f9;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
 }
 
 .name {
-  font-size: 2rem;
-  margin-bottom: 1rem;
-  color: #f94144;
+  font-size: 24px;
+  font-weight: bold;
 }
 
 .role {
-  font-size: 1.5rem;
-  color: #4d4d4d;
+  margin-top: 10px;
+  color: blue;
+}
+
+.remove-btn {
+  margin-top: 20px;
+  padding: 10px 20px;
+  background-color: #333;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.remove-btn:hover {
+  background-color: #222;
 }
 
 .add-member-form {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 2rem;
+  flex: 1 0 100%;
+  margin: 10px;
+  padding: 20px;
+  background-color: gray;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
 }
 
-.add-member-form .form-item {
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 1rem;
+.form-item {
+  margin-bottom: 20px;
+}
+
+.page-container {
+  display: fixed;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+input[type="text"] {
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
   width: 100%;
-  max-width: 25rem;
+  box-sizing: border-box;
 }
 
-.add-member-form .form-item label {
-  font-size: 1.5rem;
-  margin-bottom: 0.5rem;
-}
-
-.add-member-form .form-item input {
-  font-size: 1.5rem;
-  padding: 0.5rem;
-  border-radius: 0.5rem;
+button[type="submit"], button[type="button"] {
+  padding: 10px 20px;
+  background-color: #333;
+  color: #fff;
   border: none;
-  background-color: #f5f5f5;
-  color: #4d4d4d;
-}
-
-.add-member-form .form-item button {
-  font-size: 1.5rem;
-  padding: 0.5rem;
-  border-radius: 0.5rem;
-  border: none;
-  background-color: #f94144;
-  color: white;
+  border-radius: 5px;
   cursor: pointer;
-  transition: all 0.2s ease;
-  margin-top: 1rem;
+  margin-top: 20px;
+  transition: background-color 0.3s ease;
 }
 
-.add-member-form .form-item button:hover {
-  background-color: #f3722c;
+button[type="submit"]:hover, button[type="button"]:hover {
+  background-color: #222;
 }
 
-.add-member {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 2rem;
-  flex: 0 1 100%;
-  max-width: 100%;
-}
-
-.add-member .name a {
-  font-size: 2rem;
-  color: #f94144;
-  text-decoration: none;
-  transition: all 0.2s ease;
-}
-
-.add-member .name a:hover {
-  color: #f3722c;
-}
-
-@media only screen and (max-width: 768px) {
-  .member {
-    flex: 0 1 calc(50% - 2rem);
-    max-width: calc(50% - 2rem);
-  }
-}
-
-@media only screen and (max-width: 576px) {
-  .member {
-    flex: 0 1 calc(100% - 2rem);
-    max-width:
-    calc(100% - 2rem);
-}
-.add-member-form .form-item {
-max-width: 100%;
-}
-
-.add-member {
-flex-direction: row;
-margin-top: 1rem;
-}
-
-.add-member .name {
-flex: 1;
-}
-
-.add-member .name a {
-font-size: 1.5rem;
-}
-
-.add-member-form .form-item button {
-margin-top: 0;
-}
+button[type="submit"]:focus, button[type="button"]:focus {
+  outline: none;
+  box-shadow: 0 0 0 2px #ccc;
 }
 </style>
